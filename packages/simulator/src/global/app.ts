717b884bc764ecb674eb75e8b5deb96f5c366e99
Michael Krog <mic@apaq.dk>
2022-02-24 08:30:23 +0100

    Adds some improvment

--- packages/simulator/src/global/app.ts
+++ packages/simulator/src/global/app.ts
@@ -1,8 +1,6 @@
 import { Appkit, IdbContentProvider } from '@appkitjs.com/core';
 import { AppkitRegistry } from '@appkitjs.com/types';
 import CoreBundle from './core-bundle';
-import { ContactProvider } from './providers/contact-provider';
-import { OrderProvider } from './providers/orders-providers';
 
 declare var window: { appkit: AppkitRegistry };
 
@@ -11,6 +9,7 @@ export default async () => {
   const appkit = Appkit();
   const appDrawer = document.createElement('sl-drawer');
   appDrawer.setAttribute('placement', 'bottom');
+  appDrawer.setAttribute('no-header', 'true');
   appDrawer.classList.add('app-container');
 
   document.body.appendChild(appDrawer);
@@ -27,10 +26,6 @@ export default async () => {
 
 
   appkit.registerBundle(CoreBundle);
-  appkit.registerProvider('contacts', new ContactProvider());
-  appkit.registerProvider('orders', new OrderProvider());
-
-  appkit.getSessionSettings().setString('token', 'my-token-123123');
 
   appkit.hostBuilder = (_ => {
     while (appDrawer.firstChild) {
--- packages/types/src/data.ts
+++ packages/types/src/data.ts
@@ -3,7 +3,14 @@
  * Interface for transporting data from and to apps.
  */
 export interface Data {
+    /**
+     * The uri of the data, fx. contents://files/my-folder/my-file.png
+     */
     uri: string;
+
+    /**
+     * The optional mime type of the data, fx. image/png.
+     */
     type?: string | null;
 }
 
